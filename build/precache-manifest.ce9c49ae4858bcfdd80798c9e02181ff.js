d3672c1a1c3d71d2805097356b1b0c3c2956a8d7
kshitiz Tyagi <kshitiztyagi1011@gmail.com>
2023-03-31 21:46:50 +0530

    project

---
+++ build/precache-manifest.ce9c49ae4858bcfdd80798c9e02181ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f764fd0947898b4dfb19",
+    "url": "/static/css/main.2ed5ccf6.chunk.css"
+  },
+  {
+    "revision": "f764fd0947898b4dfb19",
+    "url": "/static/js/main.f764fd09.chunk.js"
+  },
+  {
+    "revision": "72cdf105c2febde8f51e",
+    "url": "/static/js/1.72cdf105.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfe9f174d24f07af24e77bff683cc3f1",
+    "url": "/static/media/lulocleanoutlinebold-webfont.dfe9f174.woff"
+  },
+  {
+    "revision": "769a9e73ebe39b892705db559464d88f",
+    "url": "/static/media/smooth-blue-concrete-wall_53876-96203.769a9e73.avif"
+  },
+  {
+    "revision": "b329fd8e51671a0e976c5f80e56f70ff",
+    "url": "/static/media/lulocleanoutlinebold-webfont.b329fd8e.woff2"
+  },
+  {
+    "revision": "4ee571d63521beed318fa581acba6571",
+    "url": "/index.html"
+  }
+];
